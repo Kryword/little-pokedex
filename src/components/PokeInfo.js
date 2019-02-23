6b4f5f7ead859282e5ac5c5d78524d72b58fb805
Kryword <kryword@gmail.com>
2019-02-23 23:11:29 +0100

    Añadidos más badges y id del pokemon.

--- src/components/PokeInfo.js
+++ src/components/PokeInfo.js
@@ -11,18 +11,19 @@ class PokeInfo extends Component {
             <div className="poke-info center-div">
                 <img src={this.props.pokemon.sprites.front_default} alt={this.props.pokemon.name} onMouseEnter={this.handleOnMouseEnter} onMouseLeave={this.handleOnMouseLeave}/>
                 <ul>
+                    <li key="id" className="badge badge-info">#{this.props.pokemon.id}</li>
                     <li key="nombre">Nombre: {this.props.pokemon.name}</li>
                     <li key="altura">Altura: {this.props.pokemon.height/10}m</li>
                     <li key="peso">Peso: {this.props.pokemon.weight/10}kg</li>
                     <li key="tipo">Tipo:
                         {this.props.pokemon.types.map((element) => {
-                            return <span key={element.type.name} className="badge badge-info m-2">{element.type.name}</span>
+                            return <span key={element.type.name} className={"badge badge-info m-2 "} id={element.type.name}>{element.type.name}</span>
                         })}
                     </li>
-                    <li key="stats">Stats:
+                    <li key="stats" id="stats">Stats:
                         <ul>
                             {this.props.pokemon.stats.map((stat) => {
-                                return <li key={stat.stat.name}>{stat.stat.name}: <span key={stat.stat.name + "span"} className="badge badge-pill badge-secondary">{stat.base_stat}</span></li>
+                                return <li key={stat.stat.name} className="badge badge-primary m-1 ">{stat.stat.name}: <span key={stat.stat.name + "span"} className="badge badge-pill badge-secondary">{stat.base_stat}</span></li>
                             })}
                         </ul>
                     </li>
