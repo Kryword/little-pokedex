e900a96af533c4aff261ea3c14d3c4fa2ec9f32c
Kryword <kryword@gmail.com>
2019-02-24 20:31:07 +0100

    Añadidos todos los pokemons, carga infinita.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import {render} from 'react-dom'
 import './App.css';
-import PokeList from "./PokeList";
 import PokeInfo from "./PokeInfo";
+import InfiniteScroll from 'react-infinite-scroll-component'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
-  state = {
-      pokemons : [],
-      pokemonInfo: {}
-  };
-  constructor(props) {
+    state = {
+        pokemons : [],
+        pokemonInfo: {},
+        next: null
+    };
+    limit = 0
+    constructor(props) {
     super(props);
     this.state = {
         pokemons : [],
@@ -23,33 +26,41 @@ class App extends Component {
             weight: 0,
             types: [],
             stats: []
-        }
+        },
+        hasMore : true,
+        next: ""
     };
     this.showInfoHandler = this.showInfoHandler.bind(this);
-  }
+    this.fetchMoreData = this.fetchMoreData.bind(this);
+    }
 
-  render() {
+    render() {
     return (
       <div className="App">
-        <PokeList pokemons={this.state.pokemons} showPokemon={this.showInfoHandler}/>
+          <div id="pokelist" className="poke-list" ref={pokeList => this.pokeList = pokeList}>
+              <InfiniteScroll
+                  dataLength={this.state.pokemons.length}
+                  scrollableTarget={"pokelist"}
+                  next={this.fetchMoreData}
+                  hasMore={this.state.hasMore && this.state.pokemons.length < 787}
+                  loader={<div className="loader">Loading...</div>}>
+                  {this.state.pokemons.map((pokemon, index) => {
+                      return <img key={index+"-pokemon"} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`} alt={pokemon.name} onClick={() => this.showInfoHandler(pokemon)}/>
+                  })}
+              </InfiniteScroll>
+          </div>
+        {/*<PokeList pokemons={this.state.pokemons} showPokemon={this.showInfoHandler}/>*/}
         <PokeInfo pokemon={this.state.pokemonInfo}/>
       </div>
     );
-  }
+    }
 
-  componentDidMount(){
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
-        .then((response) => response.json())
-        .then(data => {
-            // eslint-disable-next-line
-            data.results.map((pokemon, index) => {
-                pokemon.sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`;
-            });
-            this.setState({pokemons: data.results})
-        });
-  }
+    componentDidMount() {
+        this.limit = Math.floor((this.pokeList.clientWidth/96) / 2 * this.pokeList.clientWidth/96); // Very hacky form to do it, clientHeight doesn't work accordingly
+        this.fetchMoreData();
+    }
 
-  showInfoHandler(pokemon){
+    showInfoHandler(pokemon){
       fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
           .then(data => data.json())
           .then((result)=>{
@@ -58,7 +69,34 @@ class App extends Component {
                   pokemonInfo: result
               })}
           )
-  }
+    }
+
+    fetchMoreData (){
+      if (this.state.next === "") {
+          fetch("https://pokeapi.co/api/v2/pokemon?limit=" + this.limit)
+              .then((response) => response.json())
+              .then(data => {
+                  // eslint-disable-next-line
+                  this.setState({
+                      pokemons: data.results,
+                      next: data.next
+                  })
+              });
+      }else if (this.state.next != null){
+          fetch(this.state.next)
+              .then((response) => response.json())
+              .then(data => {
+                  // eslint-disable-next-line
+                  data.results.map((pokemon, index) => {
+                      this.setState({
+                          pokemons: [...this.state.pokemons, pokemon],
+                          next: data.next,
+                          hasMore: data.next != null
+                      })
+                  });
+              });
+      }
+    }
 }
 
 export default App;
